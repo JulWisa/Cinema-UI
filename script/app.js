78ee4c1afa2626f7e7c63010a4a0dc7812b0d72f
Julia <wisa@bk.ru>
2019-02-11 06:41:14 +0400

    Add comments to app.js

--- script/app.js
+++ script/app.js
@@ -1,6 +1,11 @@
 import {initStorage, reduceStorage} from "./localStorage";
 import {initMain} from "./scheduleInit";
 
+//initialize localStorage with new generated values
 initStorage();
+
+//remove too old sessions from localStorage
 reduceStorage();
+
+//initialize <main>
 initMain();
